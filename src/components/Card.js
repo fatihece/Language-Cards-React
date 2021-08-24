3ed203f1e79679ff6a65bb8b36ba6302463d27a8
fatih <fatihenaz@gmail.com>
2021-08-24 23:04:08 +0300

    Item.js added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,14 @@
-import React,{useState} from 'react';
+import React from 'react';
 import logo from "../assets/react.svg";
 import { categories } from "../helper/data"
 import "./Card.css"
+import Item from "./Item"
 
 
 
 
 const Card = () => {
-    const[showInfo,setShowInfo] = useState(false)
-
+    
     return (
 
         <div className="container">
@@ -17,26 +17,8 @@ const Card = () => {
             </div>
             <div className="card-container">
                 <h3>Languages</h3>
-                {categories.map((card,index) => {
-                    return(
-                        
-                        <div className="card" onClick={() => alert("hey mama")}>
-                            <div className="image">
-                                <img src={card.img} alt={card.name} />
-                            </div>
-                            <div className="title">
-                                <h5>{card.name }</h5>
-                            </div>
-
-                            <div className="info">
-                                <ul>
-                                    <li>{card.options[0]}</li>
-                                    <li>{card.options[1]}</li>
-                                    <li>{card.options[2]}</li>
-                                </ul>
-                            </div>
-
-                        </div>
+                {categories.map((categoryEl,index) => {
+                    return( <Item card={categoryEl} key={index} />
                     )
                 })}
             </div>
---
+++ src/components/Item.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const Item = ({card}) => {
+    const [showLogo, setShowLogo] = useState(false);
+
+    return (
+        <div className="card" onClick={() => setShowLogo(!showLogo)}>
+            {showLogo ?
+                <>
+                    <div className="image">
+                        <img src={card.img} alt={card.name} />
+                    </div>
+                    <div className="title">
+                        <h5>{card.name}</h5>
+                    </div>
+                </>
+                :
+            
+                    <ul className="info">
+                    {card.options.map((optionEl, index) => {
+                        return <li key={index}>
+                                {optionEl}
+                            </li>
+                        })}
+                    </ul>
+            }
+        </div>        
+    )
+}
+
+export default Item;
